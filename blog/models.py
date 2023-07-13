bc0d58e5183d95d8c591fcd694ce055d1ca37048
lukas-mojzis <mojzis.lukas@gmail.com>
2023-07-13 16:57:34 +0200

    Added comment ordering/filtering methods

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,12 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def ordered_comments(self):
+        return self.comments.order_by('-created_date')
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
