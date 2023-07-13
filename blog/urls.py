e2c0259cbb14443d97e73aeee7925283afce9b30
lukas-mojzis <mojzis.lukas@gmail.com>
2023-07-13 16:55:08 +0200

    Implemented comment approval and removal

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('comment/<int:pk>/remove', views.comment_remove, name='comment_remove'),
+    path('comment/<int:pk>/approve', views.comment_approve, name='comment_approve'),
 ]
