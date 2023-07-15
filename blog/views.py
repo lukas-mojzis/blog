3c1de0a5ccdcb9ddea06930eafc4c8fb11d73564
lukas-mojzis <mojzis.lukas@gmail.com>
2023-07-15 20:52:58 +0200

    Added edited date for posts that is saved & shown

--- blog/views.py
+++ blog/views.py
@@ -43,7 +43,7 @@ def post_edit(request, pk):
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            post.edited_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
