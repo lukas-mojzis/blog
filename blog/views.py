f2471bb92662db1c18b68bf064ce76acc7ac8c1c
lukas-mojzis <mojzis.lukas@gmail.com>
2023-07-15 19:32:41 +0200

    Added specific page titles for each template/view

--- blog/views.py
+++ blog/views.py
@@ -23,6 +23,7 @@ def post_detail(request, pk):
 
 @login_required
 def post_new(request):
+    title = "New post"
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -32,10 +33,11 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form, 'title': title})
 
 @login_required
 def post_edit(request, pk):
+    title = "Edit post"
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
@@ -46,7 +48,7 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form, 'title': title})
 
 @login_required
 def post_draft_list(request):
